5a16b58e1f291c2c47e4eb4a743c1af72b999b06
Ahmed Ashraf <aashrafmohamed84@gmail.com>
2026-06-08 09:57:42 +0300

    remove some uselss things

--- test/models/product_test.rb
+++
@@ -1,9 +0,0 @@
-require "test_helper"
-
-class ProductTest < ActiveSupport::TestCase
-  test "validates presence of name" do
-    product = Product.new
-    assert_not product.save
-    assert_includes product.errors[:name], "can't be blank"
-  end
-end
